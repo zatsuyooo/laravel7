7c0b554509353b2084a7bd4c2e03512b83b53695
zatsuyooo <g0846697@gmail.com>
2022-09-28 14:34:47 +0900

    entities作成

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -13,6 +13,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
+        factory(\App\Entities\User::class)->create();
         $this->assertTrue(true);
     }
 }
